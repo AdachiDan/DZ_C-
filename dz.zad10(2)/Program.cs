cd22e75d631bb12bf5659f4303d238b701446903
Daniil T <daniilteremovskiy@gmail.com>
2023-01-01 19:46:42 +0300

    feat. dz num 10

---
+++ dz.zad10(2)/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetSecondDigit(int x)
+{
+    int desyatki = x / 10;
+    int iskomoe = desyatki % 10;  
+   // Console.WriteLine("Вторая цифра: " + iskomoe);
+    x = iskomoe;
+    return (x);
+}
+
+Console.WriteLine("Нахождение второй цифры трехзначного числа");
+Console.WriteLine("Введите трехзначное число:");
+
+int x;
+
+while (!int.TryParse(Console.ReadLine()!, out x)){
+   Console.WriteLine("Неверный ввод");
+   Console.WriteLine("Введите трехзначное число:");
+}
+
+//GetSecondDigit(x);
+Console.WriteLine("Вторая цифра: " + GetSecondDigit(x));
