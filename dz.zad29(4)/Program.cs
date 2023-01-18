749c083f29d0d4d5355f04dda9bd5e5fe51bdd4f
Daniil T <daniilteremovskiy@gmail.com>
2023-01-18 12:33:31 +0300

    feat: new sem 4, dz.zad 29

---
+++ dz.zad29(4)/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] CreateArray (int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+Console.WriteLine("Array creation");
+int[] array = CreateArray(8);
+PrintArray(array);
+Console.Write($"-> [{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}, {array[5]}, {array[6]}, {array[7]}]");
+
